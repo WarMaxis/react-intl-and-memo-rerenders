34e97ebaa493636ce4cced2f30fef0eeb769807a
Michał Milanowski <mmilanow@intercars.eu>
2021-05-19 18:24:53 +0200

    feat: examples of re-renders with React-intl and memo

---
+++ docs/precache-manifest.0ff85e64514414a18b8f125e6e7f21a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133ad421806d77bf396262fcdbf7c4ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe856c655f42032bad28",
+    "url": "/static/css/main.d8002bb6.chunk.css"
+  },
+  {
+    "revision": "4f6bb5687f504ad51d0b",
+    "url": "/static/js/2.1b2e9c6a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1b2e9c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe856c655f42032bad28",
+    "url": "/static/js/main.ca6e8819.chunk.js"
+  },
+  {
+    "revision": "89b3b2ed21a73a6d7271",
+    "url": "/static/js/runtime-main.33e3a4c8.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from "react";
-import {FormattedMessage, FormattedHTMLMessage} from 'react-intl';
+import React, { useState, useEffect, memo } from "react";
+import { injectIntl } from 'react-intl';
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        <FormattedHTMLMessage
-          id="app.text"
-          defaultMessage="Hello, welcome {user}"
-          description="Welcome message"
-          values={{ user: "John Doe" }}/>
-      </p>
-      <a href="https://www.positronx.io" target="_blank" rel="noopener noreferrer">
-        <FormattedMessage
-          id="app.link"
-          defaultMessage="Go to link"
-          description="Check website"/>
-      </a>
-    </div>
-  );
+let normalComponentCounter = 0;
+let memoComponentCounter = 0;
+let injectIntlComponentCounter = 0;
+let injectIntlInsideMemoComponentCounter = 0;
+let normalComponentPlusPropIntlCounter = 0;
+
+const NormalComponent = () => {
+    normalComponentCounter = normalComponentCounter + 1;
+
+    return <div>Normal component (without injectIntl and without memo() ): <strong>{normalComponentCounter}</strong> step</div>
+};
+
+const MemoComponent = memo(() => {
+    memoComponentCounter = memoComponentCounter + 1;
+
+    return <div>Component wrapped in memo(): <strong>{memoComponentCounter}</strong> step</div>
+});
+
+const ComponentWithInjectIntlAndMemoInside = injectIntl(memo(((intl) => {
+    injectIntlComponentCounter = injectIntlComponentCounter + 1;
+
+    console.log(intl)
+
+    return <div>Component with injectIntl and memo() inside: <strong>{injectIntlComponentCounter}</strong> step</div>
+})));
+
+const ComponentWithMemoAndInjectIntlInside = memo(injectIntl((intl) => {
+    injectIntlInsideMemoComponentCounter = injectIntlInsideMemoComponentCounter + 1;
+
+    console.log(intl)
+
+    return <div>Component with memo() and injectIntl() inside: <strong>{injectIntlInsideMemoComponentCounter}</strong> step</div>
+}));
+
+const NormalComponentPlusPropIntl = memo((intl) => {
+    normalComponentPlusPropIntlCounter = normalComponentPlusPropIntlCounter + 1;
+
+    console.log(intl)
+
+    return <div>Normal component with prop intl object: <strong>{normalComponentPlusPropIntlCounter}</strong> step</div>
+});
+
+function App(intl) {
+    const [counter, setCounter] = useState(1);
+
+    useEffect(
+        () => {
+            const interval = setInterval(() => setCounter(prevState => prevState + 1), 1000);
+
+            return () => {
+                clearInterval(interval)
+            }
+        }, [setCounter]
+    )
+
+    return (
+        <div className="App">
+            <h2>React-intl injectIntl with memo() and useMemo() re-renders behaviour test</h2>
+            <h2>Counter steps: {counter}</h2>
+            <NormalComponent />
+            <br />
+            <MemoComponent />
+            <br />
+            <ComponentWithInjectIntlAndMemoInside />
+            <br />
+            <ComponentWithMemoAndInjectIntlInside />
+            <br />
+            <NormalComponentPlusPropIntl intl={intl} />
+        </div>
+    );
 }
 
-export default App;
+export default injectIntl(App);
